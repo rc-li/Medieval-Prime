7c04836db4e6d5f710837a67236630712cea358d
rc-li <ruicheng.li@uconn.edu>
2020-02-11 12:42:24 -0800

    littleBody is moving!
    yay

---
+++ Assets/Scripts/RayController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayController : MonoBehaviour
+{
+
+	public float moveSpeed;
+	public float jumpForce;
+
+	private Rigidbody2D myRigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2(moveSpeed,myRigidBody.velocity.y);
+    }
+}
