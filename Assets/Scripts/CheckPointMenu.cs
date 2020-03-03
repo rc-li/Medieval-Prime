c8aef5037ca7cf665127db37e5d8fdcda6168e0f
Weitong Wu <weitongw@usc.edu>
2020-03-03 14:48:30 -0800

    UI changes

---
+++ Assets/Scripts/CheckPointMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckPointMenu : MonoBehaviour
+{
+    public float timeStart = 10;
+    public Text textBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (Mathf.Round(timeStart) == 0) {
+            Time.timeScale = 0f;
+
+            int choice = Random.Range(1,3);
+            Debug.Log(choice.ToString());
+            Debug.Log("CHANGE");
+        } else {
+            timeStart -= Time.deltaTime;
+            textBox.text = Mathf.Round(timeStart).ToString();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/StoreMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreMenu : MonoBehaviour
+{
+    public GameObject img;
+    public int coins = 500;
+    public Text textBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void ChangeImage()
+    {
+        RectTransform rt = img.GetComponent<Image>().rectTransform;
+        float w = rt.rect.width + 50;
+        float h = rt.rect.height + 50;
+        img.GetComponent<Image>().rectTransform.sizeDelta = new Vector2(w, h);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Upgrade()
+    {
+        if (coins < 100) {
+            Debug.Log("NOT ENOUGH COINS");
+        } else {
+            coins -= 100;
+            textBox.text = coins.ToString();
+            ChangeImage();
+        }
+        
+    }
+}
