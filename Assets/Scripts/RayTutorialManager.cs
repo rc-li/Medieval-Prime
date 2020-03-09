7fe735d7f51c3156b01db9cd62f96f1fec4adb61
rc-li <husky.li.ruicheng@gmail.com>
2020-03-09 02:22:52 -0700

    the player control desynchronized
    fixing and redefining the move 1 and move 2

--- Assets/Scripts/RayTutorialManager.cs
+++ Assets/Scripts/RayTutorialManager.cs
@@ -1,7 +1,7 @@
-﻿using System.Collections;
+﻿using System.Security.Cryptography;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System.Security;
 using UnityEngine;
 
 public class RayTutorialManager : MonoBehaviour {
@@ -9,13 +9,19 @@ public class RayTutorialManager : MonoBehaviour {
 	public int popUpIndex;
 	public float waitTime;
 	public bool tutorial_1;
+	public bool tutorial_2;
 	public Rigidbody2D playerRigidBody;
+	public int gravityScale;
+	public int runSpeed;
+	public int jumpForce;
+	public RayController playerController;
 
-	void Awake() {
+	void Awake () {
 		playerRigidBody = GameObject.Find ("player").GetComponent<Rigidbody2D> ();
+		playerRigidBody.velocity = new Vector2(runSpeed,playerRigidBody.velocity.y);
+		playerController = GameObject.Find("player").GetComponent<RayController>();
+		
 	}
-
-
 
 	void Update () {
 
@@ -27,31 +33,53 @@ public class RayTutorialManager : MonoBehaviour {
 			}
 		}
 
-		if (popUpIndex == 0) {
-			if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.Mouse0)) {
-				tutorial_1 = true;
-			}
-		}
 
-		// 	for the first tutorial
-		if (tutorial_1 == true) {
+        // the player walk towards the short obstacle
+        if (popUpIndex == 0) {
+
 			if (waitTime <= 0) {
+				playerRigidBody.velocity = new Vector2 (0, 0);
 				playerRigidBody.gravityScale = 0;
-				playerRigidBody.velocity = new Vector2 (playerRigidBody.velocity.x, 0);
-				waitTime = 0.25f;
-				tutorial_1 = false;
 				popUpIndex++;
+				tutorial_1 = true;
 			} else {
+				playerRigidBody.velocity = new Vector2(runSpeed,playerRigidBody.velocity.y);
 				waitTime -= Time.deltaTime;
 			}
 
 		}
+		
+
+		// the player click jump before the short obstacle
+		if (tutorial_1) {
+			if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0)) {
+				playerRigidBody.velocity = new Vector2(runSpeed, jumpForce);
+				playerRigidBody.gravityScale = gravityScale;
+				popUpIndex++;
+				tutorial_1 = false;
+
+            }
+
+		}
+
+		print(popUpIndex);
+		// the scene should be asking the user to perform a double jump now
 
 		if (popUpIndex == 1) {
 			if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.Mouse0)) {
-				playerRigidBody.gravityScale = 5;
-				playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, 5);
-			}
+				if (waitTime <= 0) {
+					playerRigidBody.gravityScale = 0;
+					playerRigidBody.velocity = new Vector2 (playerRigidBody.velocity.x, 0);
+					waitTime = 0.25f; //set the wait time to be used in next scene
+					tutorial_1 = false;
+					popUpIndex++; // turn the page
+				} else {
+					waitTime -= Time.deltaTime;
+				}
+			} 
+			//else {
+			//	playerRigidBody.velocity = new Vector2(runSpeed, playerRigidBody.velocity.y);
+   //         }
 		}
 
 	}
