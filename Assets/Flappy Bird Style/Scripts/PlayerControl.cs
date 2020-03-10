46e5f266316c49bf76c4028d912fc0603d83ff14
Weitong Wu <weitongw@usc.edu>
2020-03-10 11:40:20 -0700

    touch reflected by circle + tutoria UI changes

--- Assets/Flappy Bird Style/Scripts/PlayerControl.cs
+++ Assets/Flappy Bird Style/Scripts/PlayerControl.cs
@@ -69,7 +69,7 @@ public class PlayerControl : MonoBehaviour
 				// MOBILE CONTROLS
 				// Disable play control when upper left corner pressed (for pause button)
 				if (!(touch.position.x < Screen.width/4 && touch.position.y > Screen.width/5)){
-					Debug.Log("TOUCH:" + touch.position.x.ToString() + " " + touch.position.y.ToString());
+					// Debug.Log("TOUCH:" + touch.position.x.ToString() + " " + touch.position.y.ToString());
 					if (touch.position.x < Screen.width/2)
 					{
 						if (touch.phase == TouchPhase.Began && !grounded)
---
+++ Assets/Scripts/multipleTouch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class multipleTouch : MonoBehaviour
+{
+    public GameObject circle;
+    public List<touchLocation> touches = new List<touchLocation>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        int i = 0;
+        while (i < Input.touchCount) {
+            Touch t = Input.GetTouch(i);
+            if (t.phase == TouchPhase.Began){
+                Debug.Log("TOUCH: began");
+                touches.Add(new touchLocation(t.fingerId, createCircle(t)));
+            } else if (t.phase == TouchPhase.Ended){
+                Debug.Log("TOUCH: ended");
+                touchLocation thisTouch = touches.Find(touchLocation => touchLocation.touchId == t.fingerId);
+                Destroy(thisTouch.circle);
+                touches.RemoveAt(touches.IndexOf(thisTouch));
+            }
+            ++i;
+        }
+    }
+
+    Vector2 getTouchPosition(Vector2 touchPosition){
+        return GetComponent<Camera>().ScreenToWorldPoint(new Vector3(touchPosition.x, touchPosition.y, transform.position.z));
+    }
+
+    GameObject createCircle(Touch t){
+        GameObject c = Instantiate(circle) as GameObject;
+        c.name = "Touch" + t.fingerId;
+        c.transform.position = getTouchPosition(t.position);
+        return c;
+    }
+}
