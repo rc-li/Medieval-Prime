7b621e9ed299bd86bbd7faf5cbb288a592fda654
Weitong Wu <weitongw@usc.edu>
2020-04-20 16:16:06 -0700

    house updates
    changed main menu, store menu, add several assests

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject town;
+
     public void PlayGame ()
     {
         PlayerPrefs.DeleteKey("Tutorial");
@@ -17,6 +20,43 @@ public class MainMenu : MonoBehaviour
         {
             Debug.Log("TUTORIAL:" + "skip tutorial");
             SceneManager.LoadScene("Main");
+        }
+    }
+
+    void Start(){
+        if (PlayerPrefs.GetInt("Town", 0) == 0){
+            PlayerPrefs.SetInt("Town", 1);
+        }
+        showTown();
+    }
+
+    void showTown(){
+        Debug.Log("town");
+        Sprite house1 = Resources.Load<Sprite>("Buildings/house_25");
+        Sprite house2 = Resources.Load<Sprite>("Buildings/house_26");
+        Sprite house3 = Resources.Load<Sprite>("Buildings/house_27");
+        Sprite house4 = Resources.Load<Sprite>("Buildings/house_29");
+        Sprite house5 = Resources.Load<Sprite>("Buildings/house_30");
+        Sprite house6 = Resources.Load<Sprite>("Buildings/house_28");
+
+        if(PlayerPrefs.GetInt("Town", 0) == 1){
+            Debug.Log("town 1");
+            town.GetComponent<Image>().sprite = house1;
+        } if(PlayerPrefs.GetInt("Town", 0) == 2){
+            Debug.Log("town 2");
+            town.GetComponent<Image>().sprite = house2;
+        } if(PlayerPrefs.GetInt("Town", 0) == 3){
+            Debug.Log("town 3");
+            town.GetComponent<Image>().sprite = house3;
+        } if(PlayerPrefs.GetInt("Town", 0) == 4){
+            Debug.Log("town 4");
+            town.GetComponent<Image>().sprite = house4;
+        } if(PlayerPrefs.GetInt("Town", 0) == 5){
+            Debug.Log("town 5");
+            town.GetComponent<Image>().sprite = house5;
+        } if(PlayerPrefs.GetInt("Town", 0) == 6){
+            Debug.Log("town 6");
+            town.GetComponent<Image>().sprite = house6;
         }
     }
 
---
+++ Assets/Scripts/TownMenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TownMenu : MonoBehaviour
+{
+    public GameObject town;
+
+    public int coins;
+    public Text coinText;
+
+    public int townLevel;
+    public Text levelText;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        coins = PlayerPrefs.GetInt("TotalCoin", 0);
+        townLevel = PlayerPrefs.GetInt("Town", 0);
+
+        coinText.text = coins.ToString();
+        levelText.text = townLevel.ToString();
+
+        showTown();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Upgrade()
+    {
+        if (coins < townLevel * 100) {
+            Debug.Log("NOT ENOUGH COINS");
+        } else {
+            coins -= townLevel * 100;
+            townLevel += 1;
+            PlayerPrefs.SetInt("TotalCoin", coins);
+            PlayerPrefs.SetInt("Town", townLevel);
+            PlayerPrefs.Save();
+            coinText.text = coins.ToString();
+            levelText.text = townLevel.ToString();
+
+            showTown();
+        }
+    }
+
+    void showTown(){
+        Sprite house1 = Resources.Load<Sprite>("Buildings/house_25");
+        Sprite house2 = Resources.Load<Sprite>("Buildings/house_26");
+        Sprite house3 = Resources.Load<Sprite>("Buildings/house_27");
+        Sprite house4 = Resources.Load<Sprite>("Buildings/house_29");
+        Sprite house5 = Resources.Load<Sprite>("Buildings/house_30");
+        Sprite house6 = Resources.Load<Sprite>("Buildings/house_28");
+
+        if(PlayerPrefs.GetInt("Town", 0) == 1){
+            town.GetComponent<Image>().sprite = house1;
+        } if(PlayerPrefs.GetInt("Town", 0) == 2){
+            town.GetComponent<Image>().sprite = house2;
+        } if(PlayerPrefs.GetInt("Town", 0) == 3){
+            town.GetComponent<Image>().sprite = house3;
+        } if(PlayerPrefs.GetInt("Town", 0) == 4){
+            town.GetComponent<Image>().sprite = house4;
+        } if(PlayerPrefs.GetInt("Town", 0) == 5){
+            town.GetComponent<Image>().sprite = house5;
+        } if(PlayerPrefs.GetInt("Town", 0) == 6){
+            town.GetComponent<Image>().sprite = house6;
+        }
+    }
+}
