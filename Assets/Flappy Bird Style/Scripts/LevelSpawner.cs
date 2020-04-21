a8f14f9659adbd77ed95b29b666ee5e9a55bad97
Furkan <furkankilicaslan@hotmail.com>
2020-04-21 03:18:02 -0700

    Demo Put Together

--- Assets/Flappy Bird Style/Scripts/LevelSpawner.cs
+++ Assets/Flappy Bird Style/Scripts/LevelSpawner.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class LevelSpawner : MonoBehaviour
 {
     public GameObject[] hazardChunks;
+    public Sprite[] spriteList;
     public GameObject town;
+    SpriteRenderer townSprite;
     [SerializeField]
-    private float spawnXPosition = 10f;
     private int spawnCount = 0;
     int randomInt;
     void Start()
     {
         InvokeRepeating("spawnChunk", 3.0f, 5f);
+        townSprite = town.GetComponent<SpriteRenderer>();
     }
 
     void spawnChunk()
@@ -25,7 +27,10 @@ public class LevelSpawner : MonoBehaviour
         }
         else
         {
+            // Move town to new position
             town.transform.position = new Vector3(35f, -2f, 0f);
+            // Change sprite of town here
+            townSprite.sprite = spriteList[Random.Range(0, spriteList.Length)];
             spawnCount = 0;
         }
         
