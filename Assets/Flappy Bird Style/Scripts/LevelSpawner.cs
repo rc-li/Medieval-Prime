d0945bf589c3727ec4aa68e21a2dd0761d6112b6
Furkan <furkankilicaslan@hotmail.com>
2020-04-25 18:22:43 -0700

    UI and Town Sprite Changes

--- Assets/Flappy Bird Style/Scripts/LevelSpawner.cs
+++ Assets/Flappy Bird Style/Scripts/LevelSpawner.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class LevelSpawner : MonoBehaviour
 {
-    public GameObject[] hazardChunks;
-    public Sprite[] spriteList;
-    public GameObject town;
-    SpriteRenderer townSprite;
     [SerializeField]
+    private GameObject[] hazardChunks;
+    [SerializeField]
+    private Sprite[] spriteList;
+    [SerializeField]
+    private GameObject town;
+    private Sprite randomTownSprite;
     private int spawnCount = 0;
     int randomInt;
     void Start()
     {
         InvokeRepeating("spawnChunk", 3.0f, 5f);
-        townSprite = town.GetComponent<SpriteRenderer>();
     }
 
     void spawnChunk()
@@ -28,9 +29,11 @@ public class LevelSpawner : MonoBehaviour
         else
         {
             // Move town to new position
-            town.transform.position = new Vector3(35f, -2f, 0f);
+            town.transform.position = new Vector3(40f, 0f, 0f);
             // Change sprite of town here
-            townSprite.sprite = spriteList[Random.Range(0, spriteList.Length)];
+            randomTownSprite = spriteList[Random.Range(0, spriteList.Length)];
+            town.GetComponent<SpriteRenderer>().sprite = randomTownSprite;
+            town.GetComponent<SpriteRenderer>().sortingLayerName = "Background";
             spawnCount = 0;
         }
         
